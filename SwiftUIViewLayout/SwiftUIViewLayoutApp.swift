b31a1ecf998f123ff91b5217fa6f837309adeaa1
zoop1259 <kdm432191@gmail.com>
2022-06-14 02:13:06 +0900

    Initial Commit

---
+++ SwiftUIViewLayout/SwiftUIViewLayoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIViewLayoutApp.swift
+//  SwiftUIViewLayout
+//
+//  Created by 강대민 on 2022/06/14.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIViewLayoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
