3b8c94925b3ce08c814930a61db9a9dee8778db9
zoop1259 <kdm432191@gmail.com>
2022-06-14 23:21:43 +0900

    layout fin

--- SwiftUIViewLayout/ContentView.swift
+++ SwiftUIViewLayout/ContentView.swift
@@ -9,8 +9,63 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+//        Text("hello")
+//        //이젠 된다..
+//        Text("이러면 컴파일안됨")
+        /*
+         containver {
+            content
+            content
+            ...
+         }
+         */
+        
+        //basic stack
+        HStack(spacing: 30) {
+            VStack(spacing: 50) {
+                Rectangle()
+                    .fill(Color.yellow)
+                    .frame(width: 150, height: 150)
+                Rectangle()
+                    .fill(Color.green)
+                    .frame(width: 150, height: 150)
+            }
+            //Z스택은 spacing불가.
+            ZStack() {
+                Rectangle()
+                    .fill(Color.blue)
+                    .frame(width: 150, height: 150)
+                Rectangle()
+                    .fill(Color.brown)
+                    .frame(width: 150, height: 150)
+                    //z스택은 xy좌표 둘다 필요
+                    .offset(x: 40, y: 40)
+            }
+        }
+        
+        //alignment top, center, bottom
+        //위에처럼 스탁 자체에도 걸 수 있으나 컨테이너에 걸수도 있다.
+        HStack(alignment: .bottom) {
+        //HStack() {
+            Spacer() //빈공간 채우기
+            Rectangle().fill(Color.green).frame(width: 150, height: 150)
+            Rectangle().fill(Color.red)
+                .frame(width: 150, height: 550)
+        }.background(Color.blue)
+        
+        //게시글 작업?
+        VStack {
+            Text("제목").font(.largeTitle)
+            Text("부제목").foregroundColor(Color.gray)
+            Spacer()
+            Text("본문내용")
+            Spacer()
+            Spacer()
+            Text("각주").font(.body)
+            
+        }
+        
+        
     }
 }
 
